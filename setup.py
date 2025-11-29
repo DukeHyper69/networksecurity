dd5f43e5353fe48f49806bed16ea7004ff564b3b
DukeHyper69 <zenithjung098@gmail.com>
2025-11-29 22:28:11 +0545

    Setuping Setup.py File

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup
+from typing import List
+import os
+import sys
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    """This function will return list of requirements"""
+    try:
+        requirements:List[str]= []
+        with open(file_path) as file_obj:
+            #Read Line
+            requirements=file_obj.readlines()
+            # Process and ignore the extra line
+            requirements = [req.replace("\n","") for req in requirements]            
+            if HYPEN_E_DOT in requirements:
+                requirements.remove(HYPEN_E_DOT)
+        return requirements            
+    except FileNotFoundError:
+        print("requirements.txt file not Found")
+            
+setup(
+    name="NetworkSecurity",
+    version='0.0.1',
+    author='Zenithjung',
+    author_email='canzenith098@gmail.com',
+    packages= find_packages(),
+    install_requires = get_requirements('requirements.txt')
+    
+)
